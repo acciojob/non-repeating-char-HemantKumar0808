b5bc0c6443e22503b943edc32420fc58194fc1eb
Hemant Kumar <hemusharma10001@gmail.com>
2025-10-15 19:37:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	for(let i = 0; i < str.length; i+=2){
+		if(str[i] !== str[i+1]){
+			return str[i];
+		}
+	}
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
